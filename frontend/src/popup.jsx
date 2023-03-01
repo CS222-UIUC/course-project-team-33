b527fc50bf5e2210c0b9851da40f8a01d92eb0ab
Tianwei-Qing <tqing3@illinois.edu>
2023-03-01 12:06:21 -0600

    set up eslint

--- frontend/src/popup.jsx
+++ frontend/src/popup.jsx
@@ -1,15 +1,15 @@
-import React from "react";
-import { render } from "react-dom";
+import React from 'react';
+import { render } from 'react-dom';
 
-import './popup.css'
+import './popup.css';
 
 function Popup() {
     return (
-        <div className="entrance"> 
+        <div className="entrance">
             <h1 className="app-name"> Multi-Language Summarizer </h1>
             <p> your text here </p>
         </div>
     );
 }
 
-render(<Popup />, document.getElementById("react-target"));
+render(<Popup />, document.getElementById('react-target'));

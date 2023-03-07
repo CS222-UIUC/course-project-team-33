85eff2667cb17cac48833d8529d6314a1fc0a982
Zihan Ming (Steven) <73292427+Zihanming@users.noreply.github.com>
2023-03-06 21:06:06 -0600

    update README

--- backend/MultiLangTextSumGoogleExtension/MultiLangApp/views.py
+++ backend/MultiLangTextSumGoogleExtension/MultiLangApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+import json
+from django.contrib.auth.models import User #####
+from django.http import JsonResponse , HttpResponse ####
+
+
+def home(request):
+    return HttpResponse('<h1>Blog Home</h1>')

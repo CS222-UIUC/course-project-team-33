f522217e008c7b492acff3304b10089492242d6d
Zihan Ming <zihanm4@illinois.edu>
2023-04-19 13:23:06 -0500

    Merge two function

--- backend/MultiLangProject/MultiLangApp/views.py
+++ backend/MultiLangProject/MultiLangApp/views.py
@@ -50,3 +50,44 @@ def translate(request):
     print('json-data to be sent: ', res)
 
     return JsonResponse(res)
+
+
+#combine two above
+def translate_and_summarize(request):
+    if request.method == 'PUT':
+        article = request.data.get('article', None)
+        lang = request.data.get('language', None)
+    else:
+        article = request.GET.get('article', None)
+        lang = request.GET.get('language', None)
+
+    auth_key = "da19e392-2688-f41f-38d5-5389e9ad7b56:fx"  # Replace with your key
+    translator = deepl.Translator(auth_key)
+
+    try:
+        # Translate article
+        result = translator.translate_text(article, target_lang=lang)
+        translation = {
+            'translation': result.text,
+            'raw': 'Successful',
+        }
+
+        # Get summary of translated article
+        summarizer = pipeline("summarization")
+        summary = summarizer(translation['translation'], max_length=130, min_length=30, do_sample=False)[0]
+        summary_data = {
+            'summary': summary['summary_text'],
+            'raw': 'Successful',
+        }
+
+        data = {
+            'translation': translation['translation'],
+            'summary': summary_data['summary'],
+            'raw': translation['raw'],
+        }
+    except:
+        data = {
+            'error': 'Translation or summarization failed',
+        }
+
+    return JsonResponse(data)
